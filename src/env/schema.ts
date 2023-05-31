4c5459126b3d15de5fe715b9c4d7aedbfda7c020
Ludwig Nagelhus <ludwignagelhus@gmail.com>
2023-05-31 16:11:09 +0200

    Disable auth temporarily

--- src/env/schema.ts
+++ src/env/schema.ts
@@ -1,8 +1,8 @@
 import {z} from "zod";
 
 export const serverSchema = z.object({
-    AUTH_SECRET: z.string(),
-    AZURE_AD_CLIENT_SECRET: z.string(),
-    AZURE_AD_CLIENT_ID: z.string(),
-    AZURE_AD_TENANT_ID: z.string(),
+    // AUTH_SECRET: z.string(),
+    // AZURE_AD_CLIENT_SECRET: z.string(),
+    // AZURE_AD_CLIENT_ID: z.string(),
+    // AZURE_AD_TENANT_ID: z.string(),
 })

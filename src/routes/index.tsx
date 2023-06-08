e079a6a108b888c64b082e855f9dab566287bf64
Simen Abelsen <simen@sentinel.no>
2023-06-08 12:33:54 +0200

    :passport_control: Disable auth for now (#6)

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,40 +1,11 @@
 import { A } from "solid-start";
 
-import { signIn, signOut } from "@auth/solid-start/client";
-import Counter from "~/components/Counter";
-import { useSession } from "~/utils";
-
 export default function Home() {
-  const session = useSession();
-
-  const user = () => session()?.user;
-
   return (
     <main class="text-center mx-auto text-gray-700 p-4">
       <h1 class="max-6-xs text-6xl text-sky-700 font-thin uppercase my-16">
-        Hello world!
+        Hello, Bertil
       </h1>
-      <Counter />
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
-      <p>
-        <button onClick={() => signIn("azure-ad")}>Login with Azure</button>
-      </p>
-      <p>
-        <button onClick={() => signOut("azure-ad")}>Logout</button>
-      </p>
-      <div>
-        <pre>{JSON.stringify(user(), null, 2)}</pre>
-      </div>
       <p class="my-4">
         <span>Home</span>
         {" - "}
--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,46 @@
 import solid from "solid-start/vite";
 import { defineConfig } from "vite";
 import { VitePWA, VitePWAOptions } from "vite-plugin-pwa";
-import vercel from "solid-start-vercel"
-
+import vercel from "solid-start-vercel";
 
 const pwaOptions: Partial<VitePWAOptions> = {
-  base: '/',
-  includeAssets: ['favicon.ico', 'robots.txt'],
+  base: "/",
+  includeAssets: ["favicon.ico", "robots.txt"],
   manifest: {
-    name: 'Carweb GO',
-    short_name: 'CWGO',
-    description: 'A PWA for Carweb using Solid and Vite',
-    theme_color: '#1b1c1d',
+    name: "Carweb GO",
+    short_name: "CWGO",
+    description: "A PWA for Carweb using Solid and Vite",
+    theme_color: "#1b1c1d",
     icons: [
       {
-        src: 'android-chrome-192x192.png',
-        sizes: '192x192',
-        type: 'image/png',
+        src: "android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
       },
       {
-        src: 'android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
+        src: "android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
       },
       {
-        src: 'android-chrome-512x512.png',
-        sizes: '512x512',
-        type: 'image/png',
-        purpose: 'any maskable',
+        src: "android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+        purpose: "any maskable",
       },
       {
-        src: 'apple-touch-icon.png',
-        size: '180x180',
-        type: 'image/png'
-      }
-    ]
+        src: "apple-touch-icon.png",
+        size: "180x180",
+        type: "image/png",
+      },
+    ],
   },
   devOptions: {
     enabled: true,
     type: "module",
   },
-}
+};
 
 export default defineConfig({
-  plugins: [
-    solid({ adapter: vercel({})}),
-    VitePWA(pwaOptions),
-  ],
+  plugins: [solid({ adapter: vercel({}) }), VitePWA(pwaOptions)],
 });

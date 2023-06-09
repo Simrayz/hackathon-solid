21f87528991f06554ca9c46dad29d9da0b66b6c1
= <simen@sentinel.no>
2023-06-09 12:04:30 +0200

    :lipstick: Adjust dashboard headings

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,14 +3,15 @@ import { AppShell } from "~/components/AppShell";
 
 export default function Home() {
   return (
-    <AppShell hideHeader>
-      <h1 class="text-2xl text-ui-title font-semibold mt-10">Hei, Bertil</h1>
-      <div class="bg-ui-border h-1 rounded-full w-16 mt-0.5 mb-2" />
-      <p class="text-xl font-semibold">
-        Hva vil du gjøre i dag? Oppgavene er
-        <span class="text-font-pink">&nbsp;klare til deg</span>.
+    <AppShell>
+      <h1 class="text-2xl text-center text-ui-title font-semibold mt-10">
+        Hei, Bertil
+      </h1>
+      <div class="bg-ui-border h-1 rounded-full w-16 mt-0.5 mb-2 mx-auto" />
+      <p class="text-xl text-center font-semibold text-font-default">
+        Hva vil du gjøre i dag?
       </p>
-      <div class="flex flex-col items-center pt-11 space-y-3">
+      <div class="flex flex-col items-center mt-12 space-y-3">
         <A
           href="/vehicles"
           class="flex justify-center py-3 bg-ui-button-orange w-56 rounded-lg"
